d4244d9a394e2ff3d90e0705773c27fc50aefaa1
AYUSH KUMAR <inc.ayush29@gmail.com>
2023-05-11 18:12:57 +0530

    Add files via upload

---
+++ scratch_77.py
@@ -0,0 +1,30 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+
+from PIL import Image
+import requests
+
+# load image from the IAM database
+#url = 'https://fki.tic.heia-fr.ch/static/img/a01-122-02-00.jpg'
+#image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+image = Image.open('C:/xampp/htdocs/website/uploads/image.png').convert("RGB")
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(generated_text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
